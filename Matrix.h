e588b376339f4626b173cd00c1e3df3b86171ecc
JakubPietraszek <jakubpietraszek02@gmail.com>
2022-09-16 11:14:50 +0200

    Add project files.

---
+++ Matrix.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include "vector.h"
+class Matrix
+{
+	std::vector<std::vector<int>> matrix;
+public:
+	void setMatrix(std::vector<std::vector<int>> matrix);
+	int& at(int x, int y);
+	Vector operator*(Vector vec);
+	Matrix generateIdentityMatrix();
+	friend bool operator == (Matrix leftOne, Matrix rightOne);
+};
+
